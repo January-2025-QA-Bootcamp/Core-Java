da68e11b1359355d8e6c58be674459b012d98bc4
TofaelKabir <tofaelkabir@gmail.com>
2025-02-04 21:19:47 -0500

    Java Basic 01

---
+++ src/lec01_java_basic/JavaComments.java
@@ -0,0 +1,47 @@
+package lec01_java_basic;
+
+// This is a single line comment
+// Single line comment is mostly used in coding
+// We mostly use it as a separate line or at the end of any line of code
+// How to create single line comment: for Windows user: we use ctrl + / 
+// for Mac user: we use command + / 
+// and to re-do, click 'control/command slash', then you will go back to previous stage
+
+
+/*
+ Java Comments:
+- The Java comments are the statements that are not executed by the compiler and interpreter. 
+- The comments can be used to provide information or explanation about the variable, method, 
+class or any statement.
+- It can also be used to hide program code.
+- 2 types --> multiple line comments and single line comment
+- This is an example of multiple line comments
+ */
+
+/*
+slash [/] + shift 8 (*) + Enter ---- Thats how you can create multiple line comments
+Multiple line comments can contain a single line too.
+multiple line comments are mostly use separately like this, not used at the end of any line of code
+ */
+
+/*
+ * The below is the most common use in the Industry
+ * Author @ Mohammad Sharkar
+ * 02/04/2025
+ * Version 1.0
+ * 
+ * Author @ Alex
+ * 02/11/2025
+ * Version 2.0
+ */
+
+
+
+public class JavaComments { // this is a class
+
+	public static void main(String[] args) { // main method
+		System.out.println("Java Comments");
+
+	}
+
+}
